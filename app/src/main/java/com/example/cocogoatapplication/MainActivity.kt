a94d8237be7d07e6395157e9d8a64a82d5f76cf2
Amiran Kurman <amiranfox@gmail.com>
2021-02-22 08:11:45 +0600

    read and save

--- app/src/main/java/com/example/cocogoatapplication/MainActivity.kt
+++ app/src/main/java/com/example/cocogoatapplication/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.cocogoatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
-        var databaseRef = firebaseDatabase.reference
+        var databaseRef = firebaseDatabase.getReference("messages")
 
-        databaseRef.setValue("Diff")
+        var employee = Employee("uamee", "Hardbass Producer",
+        "Mother Russia", 25)
+
+        databaseRef.setValue(employee)
+
+        databaseRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var value = dataSnapshot.value as HashMap<String, Any>
+                Log.d("VALUE: ", value.get("name").toString())
+            }
+
+            override fun onCancelled(error:DatabaseError) {
+                Log.d("ERROR: ", error.message)
+            }
+        })
     }
+    data class Employee(var name: String, var position: String, var homeAddress: String, var age: Int)
 }
